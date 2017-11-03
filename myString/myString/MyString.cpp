864941aff515690864c630f79e7ac37c1e78687c
mord <mord@cyberElite.jct>
2017-11-03 12:03:37 +0200

    write string program

---
+++ myString/myString/MyString.cpp
@@ -0,0 +1,108 @@
+#include "MyString.h"
+
+MyString::MyString(char* newStr)
+{
+	m_str = new char[strlen(newStr) + 1];
+	strcpy_s(m_str, sizeof m_str, newStr);
+	m_size = strlen(newStr) + 1;
+}
+
+MyString::~MyString()
+{
+	if (m_str != NULL)
+	{
+		delete m_str;
+	}
+}
+
+
+char* MyString::GetStr()
+{
+	return m_str;
+}
+
+int MyString::GetLength()
+{
+	return strlen(m_str);
+}
+
+void MyString::Assign(char* newStr)
+{
+	if (strlen(newStr) < m_size)
+	{
+		strcpy_s(m_str, sizeof m_str + 1, newStr);
+	}
+	else
+	{
+		delete m_str;
+		m_str = new char[strlen(newStr) + 1];
+		strcpy_s(m_str, sizeof m_str, newStr);
+		m_size = strlen(newStr) + 1;
+	}
+}
+
+void MyString::Append(char* newStr)
+{
+	if (strlen(m_str) + strlen(newStr) < m_size)
+	{
+		strcat_s(m_str, sizeof m_str, newStr);
+	}
+	else
+	{
+		char* tmp = new char[strlen(m_str) + strlen(newStr) + 1];
+		strcpy_s(tmp, sizeof m_str, m_str);
+		strcat_s(tmp, sizeof tmp, newStr);
+		delete m_str;
+		m_str = tmp;
+		m_size = strlen(m_str) + 1;
+	}
+}
+
+char MyString::CharAt(unsigned int index)
+{
+	char ans = NULL;
+	if (index < strlen(m_str))
+	{
+		ans = m_str[index];
+	}
+	return ans;
+}
+
+int MyString::Compare(MyString rhs, MyString lhs)
+{
+	int ans = 0;
+	if (rhs.m_str != NULL && lhs.m_str != NULL)
+	{
+		ans = strcmp(rhs.m_str, lhs.m_str);
+	}
+	else if (rhs.m_str != NULL)
+	{
+		ans = 1;
+	}
+	else
+	{
+		ans = -1;
+	}
+	return ans;
+}
+
+bool MyString::IsEmpty()
+{
+	bool ans = false;
+	if (m_str == NULL)
+	{
+		ans = true;
+	}
+	return ans;
+}
+
+void MyString::Clear()
+{
+	if (m_str != NULL)
+	{
+		delete m_str;
+		m_str = NULL;
+		m_size = 0;
+	}
+	
+}
---
+++ myString/myString/MyString.h
@@ -0,0 +1,64 @@
+#include <string.h>
+
+class MyString
+{
+public:
+	MyString(char* newStr);
+	~MyString();
+	
+	/*
+	Output: the string
+	*/
+	char* GetStr();
+
+	/*
+	Output: the actually size of the string
+	*/
+	int GetLength();
+
+	/*
+	assign new string
+	Input: the new string
+	*/
+	void Assign(char* newStr);
+
+	/*
+	appends new string to the old string
+	Input: new string
+	*/
+	void Append(char* newStr);
+
+	/*
+	Input: index of the charactor to return.
+	Output: the charactor in the index place. Or NULL if the index is bigger then the size of the string.
+	*/
+	char CharAt(unsigned int index);
+
+	/*
+	copmare between to strings.
+	Input: tow strings.
+	Output:	 1 if the left string is biggger.
+			-1 if the right string is biggger.
+			0 if there are equal.
+	*/
+	static int Compare(MyString rh, MyString lh);
+
+	/*
+	Output: True if the string is NULL.
+			False otherwise.
+	*/
+	bool IsEmpty();
+
+	/*
+	erases the string
+	*/
+	void Clear();
+
+
+private:
+	// pointer to the string
+	char* m_str;
+
+	// size of the array of m_str. 0 if the pointer is NULL.
+	unsigned int m_size;
+};
